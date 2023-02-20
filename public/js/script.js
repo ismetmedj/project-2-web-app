07e35df80826036dd9f07c76a4e17a2159ddd01f
Cedric Chatellier <cedricchatellier95.com>
2023-02-20 11:53:12 +0100

    create link whith mongod for timetable model

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,22 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+import myAPI from './service.js';
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Project-2 JS imported successfully!");
 });
+
+document.querySelector('#create').addEventListener('click', (event)=> {
+  const clone= document.querySelector('#createTT').content.cloneNode(true);
+  document.querySelector('#main').innerHTML= '';
+  document.querySelector('#main').append(clone);
+})
+
+document.querySelector('#createTT').addEventListener('click', async (event) => {
+  event.defaultPrevented();
+  const newTT= { title };
+  try {
+    const res= await myAPI.post('/timetable', newTT);
+    // console.log(res);
+  } catch (error) {
+    console.error(error);
+  }
+})
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,14 +1,33 @@
 const express = require('express');
+const isLoggedIn = require('../middleware/isLoggedIn');
 const router = express.Router();
+const User= require('../models/User.model');
+const TimeTable= require('../models/TimeTable.model');
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
-router.get('/timetable', (req, res, next) => {
+router.get('/timetable', isLoggedIn, (req, res, next) => {
   res.render('timetable');
 });
 router.get('/profile', (req, res, next) => {
   res.render('profile');
 });
+
+router.post('/timetable', isLoggedIn, async (req, res, next)=> {
+  try {
+    
+    const userId= await User.findOne({username: req.session.currentUser.username});
+    const createTT= {
+      title: req.body.title,
+      admin: userId._id,
+      participants: [],
+    }
+    const newTimeTable = await TimeTable.create(createTT);
+    res.status(201).json(newTimeTable);
+  } catch (error) {
+    next(error);
+  }
+})
 module.exports = router;
