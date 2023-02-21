c8e0f3b5b7ef991d477bcb072427b1cd5d4232bf
Cedric Chatellier <cedricchatellier95.com>
2023-02-21 15:34:46 +0100

    CRUD for event with ugly btn

--- public/js/script.js
+++ public/js/script.js
@@ -4,10 +4,12 @@ import launchTimetable from "./grid-timetable.js";
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Project-2 JS imported successfully!");
 });
-
+const url= document.location.href;
+  const id=url.slice(32);
 // 
 launchTimetable();
 createBtnEvent();
+listEvent();
 function createBtnEvent () {  
   // console.log('ded');
   const eventBtn = document.createElement("button");
@@ -30,9 +32,64 @@ const createAnEvent= async  () => {
   const content= document.querySelector('#content').value;
   const hour= document.querySelector('#hour').value;
   const day= document.querySelector('#day').value;
-  const url= document.location.href;
-  const id=url.slice(32);
-  const newEvent= { hour, day, content, title};
-  const res= await myAPI.post(`/timetable/${id}`, newEvent);
-  console.log(res.data);
+  if(title!==''){
+    const newEvent= { hour, day, content, title};
+    const res= await myAPI.post(`/timetable/${id}`, newEvent);
+  }
+  // console.log(res.data);
+  document.querySelector('#divBtn').innerHTML= "";
+  createBtnEvent();
+  listEvent();
+  // const listDiv= document.querySelector('div');
+  
+}
+async function listEvent(){
+  const ul= document.querySelector('#ullist');
+  ul.innerHTML= '';
+  const allEvent= await myAPI.get(`/timetable/${id}/event`);
+  allEvent.data.forEach((oneEvent) => {
+  const li= document.createElement('li');
+  li.setAttribute('id', oneEvent._id);
+  const delBtn= document.createElement('button');
+  delBtn.textContent= "Delete";
+  delBtn.addEventListener('click', (event) => deleteEvent(oneEvent));
+  const ediBtn= document.createElement('button');
+  ediBtn.textContent= "Edit";
+  ediBtn.addEventListener('click', (event) => editForm(oneEvent));
+  li.textContent= oneEvent.title+ ': '+ oneEvent.content;
+  li.append(delBtn, ediBtn);
+  ul.append(li);
+  })
+}
+
+async function editForm(oneEvent) {
+  const clone = document
+  .querySelector("#createEvent")
+  .content.cloneNode(true);
+  document.getElementById(oneEvent._id).innerHTML= ''
+  document.getElementById(oneEvent._id).append(clone);
+  // console.log(document.querySelector('#newEvent>#title'));
+  document.querySelector('#newEvent>#title').setAttribute('value', oneEvent.title);
+  document.querySelector('#newEvent>#content').setAttribute('value', oneEvent.content);
+  document.querySelector(`#newEvent>#hour`).value= oneEvent.hour;
+  document.querySelector(`#newEvent>#day`).value= oneEvent.day;
+  document.querySelector('#addAnEvent').addEventListener('click', (event) => editEvent(oneEvent));
+}
+async function editEvent(oneEvent){
+  const updatedEvent= {
+    title: document.querySelector('#newEvent>#title').value,
+    content: document.querySelector('#newEvent>#content').value,
+    hour: document.querySelector(`#newEvent>#hour`).value,
+    day: document.querySelector(`#newEvent>#day`).value,
+    timeTable: id,
+  }
+  console.log('ok');
+  await myAPI.patch(`timetable/${id}/event/${oneEvent._id}`, updatedEvent);
+  console.log('ok');
+  await listEvent();
+}
+async function deleteEvent(event) {
+  await myAPI.delete(`/timetable/event/${event._id}`);
+  // console.log('ok')
+  await listEvent();
 }
