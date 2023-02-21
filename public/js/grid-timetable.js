3eaafcd8b58184ffd60cfc0205edab54781892bc
lekix-lekix <114491324+lekix-lekix@users.noreply.github.com>
2023-02-21 11:56:36 +0100

    inserted dataset into each div (#14)

--- public/js/grid-timetable.js
+++ public/js/grid-timetable.js
@@ -5,14 +5,23 @@ const rows = 11;
 let cells = [];
 
 function createTheGrid() {
-  for (let i = 1; i < columns * rows; i++) {
-    createCell();
+  let x = 0;
+  let y = 0;
+  for (let i = 0; i < columns * rows; i++) {
+    if (i % columns === 0 && i > 0) {
+      x = 0;
+      y++;
+    }
+    createCell(x, y);
+    x++;
   }
 }
 
-function createCell() {
+function createCell(x, y) {
   const div = document.createElement("div");
   div.classList.add("cell");
+  div.dataset.x = x;
+  div.dataset.y = y;
   gridElement.append(div);
   cells.push(div);
 }
@@ -41,7 +50,7 @@ function createHours() {
 
 function launchTimetable() {
   createTheGrid();
-  createCell();
+
   createDays();
   createHours();
 }
