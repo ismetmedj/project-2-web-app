ac9bc4db86f143358b72b2cdc10abf4b85e3dda4
Cedric Chatellier <cedricchatellier95.com>
2023-02-22 10:00:57 +0100

    nt

--- public/js/grid-timetable.js
+++ public/js/grid-timetable.js
@@ -41,27 +41,27 @@ function createDays() {
     const days = copy[i];
     days.classList.add("days");
   }
-  for(let i=1; i<82; i+=8){
-    copy[i].classList.add('d0');
-  }
-  for(let i=2; i<83; i+=8){
-    copy[i].classList.add('d1');
-  }
-  for(let i=3; i<84; i+=8){
-    copy[i].classList.add('d2');
-  }
-  for(let i=4; i<85; i+=8){
-    copy[i].classList.add('d3');
-  }
-  for(let i=5; i<86; i+=8){
-    copy[i].classList.add('d4');
-  }
-  for(let i=6; i<87; i+=8){
-    copy[i].classList.add('d5');
-  }
-  for(let i=7; i<88; i+=8){
-    copy[i].classList.add('d6');
-  }
+  // for(let i=1; i<82; i+=8){
+  //   copy[i].classList.add('d0');
+  // }
+  // for(let i=2; i<83; i+=8){
+  //   copy[i].classList.add('d1');
+  // }
+  // for(let i=3; i<84; i+=8){
+  //   copy[i].classList.add('d2');
+  // }
+  // for(let i=4; i<85; i+=8){
+  //   copy[i].classList.add('d3');
+  // }
+  // for(let i=5; i<86; i+=8){
+  //   copy[i].classList.add('d4');
+  // }
+  // for(let i=6; i<87; i+=8){
+  //   copy[i].classList.add('d5');
+  // }
+  // for(let i=7; i<88; i+=8){
+  //   copy[i].classList.add('d6');
+  // }
     // for (let i = 1; i < 8; i++) {
     //     const days = copy[i]
     //     days.classList.add('days')
